13f4c477d50bef323b6681bdbb01c3bb7f534bc7
simwawatim <simwawatimothy65@gmail.com>
2026-03-09 13:18:07 +0000

    added no no

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    host: true, // listen on all interfaces (0.0.0.0)
+    port: 5174, // match your systemd port
+    strictPort: true, // fail if port is in use
+    allowedHosts: [
+      "erp-frontend.izyanehub.com", // allow your reverse proxy host
+      "localhost",
+      "127.0.0.1",
+      "192.168.100.17", // your internal IP
+      "192.168.100.37"  // optional if you access from another IP
+    ]
+  }
 });
